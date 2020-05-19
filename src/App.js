edd908ffda623156f22fd219fbf8db4287e33210
Jorge Ruiz <jorgesmacbookpro@Jorges-MacBook-Pro.local>
2020-05-19 01:14:57 -0600

    deployment commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import {
+  BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <div >
+            main landing
+          </div>
+        </Route>
+        <Route exact path="/members">
+          <div>
+            member list table
+          </div>
+        </Route>
+        <Route exact path="/activate">
+          activate home page
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
